c6eb3ed5aa00d8837302adbda4af85f2714411cd
prathamshah-09 <pratham.rs09@gmail.com>
2025-10-24 14:18:26 +0530

    Add src folder as normal files

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+// const dotenv = require('dotenv') ;
+// const serverConfig = require('./server-config');
+// dotenv.config() ;
+
+module.exports = {
+    ServerConfig: require('./server-config'),
+    logger: require('./logger-config')
+}
